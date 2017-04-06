1e1c8a1771012441c14d55d574e914de13c2c5ea
Razvan Tomegea <razvan.tomegea@gmail.com>
2017-04-06 21:13:44 +0300

    changed getting started images

--- src/pages/getting-started/getting-started.ts
+++ src/pages/getting-started/getting-started.ts
@@ -22,17 +22,17 @@ export class GettingStartedPage {
       {
         title: 'Welcome to the Health Guide!',
         description: 'Before we begin, I want to remind you that this is the Health Guide App. <br/><br/><b class="slide__description--bold">This is NOT</b> the "I want to lose weight" Guide app<br/><b class="slide__description--bold">This is NOT</b> the "I want to look good for my girl/boy" Guide app<br/><b class="slide__description--bold">This is NOT</b> the "I want to have six pack" Guide app<br/><b class="slide__description--bold">This is NOT</b> the "I want to have huge muscles" Guide app',
-        image: 'assets/icon/favicon.ico',
+        image: 'assets/img/ica-slidebox-img-1.png',
       },
       {
         title: 'What is the Health Guide App?',
         description: '<b class="slide__description--bold">The Health Guide App</b> is an application focused on lifestyle changes, based on the uniqueness of each individual\'s physical and mental constituition.<br/><br/>The app was designed to make you learn how to:<br/><b class="slide__description--bold">Develop long-term healthy habits</b><br/><b class="slide__description--bold">Listen to your body\'s needs</b><br/><b class="slide__description--bold">Become your own nutritionist and doctor</b><br/><br/>The app evolves as you evolve.',
-        image: 'assets/icon/favicon.ico',
+        image: 'assets/img/ica-slidebox-img-2.png',
       },
       {
         title: 'Why? Is it worth it?',
         description: '<b class="slide__description--bold">Every day</b>, you must remind yourself why you are doing this.<br/><b class="slide__description--bold">Every day</b>, you must remind yourself that life and health are the most important gifts we have.</br/><b class="slide__description--bold">Every day</b> you must ask yourself:<br/><br/>"Do I truly love my body?"<br/>"If I am sick, did I ever do something about it? Have I ever wanted to be healthy?"<br/>"If I am old and sick, is it because I am old or because I have never given damn about my health?"</b><br/><br/>Health is a lifetime commitment <b class="slide__description--bold">(like marriage)<b/>. If we lose our health or life, nothing else matters.',
-        image: 'assets/icon/favicon.ico',
+        image: 'assets/img/ica-slidebox-img-3.png',
       }
     ];
 

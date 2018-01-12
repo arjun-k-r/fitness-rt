0f9090bebafc826a1a69851a53fa8093099f7535
razvantomegea <RazvanT@wenglor.com>
2018-01-12 17:48:09 +0200

    removed redundant pages and added profile page

---
+++ src/models/user.ts
@@ -0,0 +1,9 @@
+export default class User {
+  constructor(
+    public birth: number,
+    public email: string,
+    public height: number,
+    public username: string,
+    public weight: number
+  ) { }
+}
